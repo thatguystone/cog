b53ddb9a433a6dfb791c9aa08c6982e47bd3639b
Andrew Stone <a@stoney.io>
2016-06-10 14:26:44 -0400

    Add stringc.Dedent

--- stringc/stringc.go
+++ stringc/stringc.go
@@ -3,9 +3,49 @@
 // "stringc" is pronounced "strings".
 package stringc
 
-import "strings"
+import (
+	"bytes"
+	"strings"
+	"unicode"
+)
 
 // Indent prefixes each line of the given string with the given prefix.
 func Indent(s, prefix string) string {
 	return prefix + strings.Replace(s, "\n", "\n"+prefix, -1)
+}
+
+// Dedent is the opposite of indent: it removes the given number of indents
+// from each line. The indent style and width is assumed to be the total
+// whitespace before the first non-whitespace character on the first line.
+//
+// This is a shortcut from DedentPrefix(s, "", n)
+func Dedent(s string, n int) string {
+	return DedentPrefix(s, "", n)
+}
+
+// DedentPrefix is the opposite of indent: it removes the given prefix n
+// number of times from each line.
+func DedentPrefix(s, prefix string, n int) (out string) {
+	if n <= 0 {
+		return s
+	}
+
+	b := bytes.NewBufferString(s)
+
+	for i := 0; b.Len() > 0; i++ {
+		l, _ := b.ReadString('\n')
+
+		if i == 0 && prefix == "" {
+			tl := strings.TrimLeftFunc(l, unicode.IsSpace)
+			prefix = l[:len(l)-len(tl)]
+		}
+
+		for j := 0; j < n && strings.HasPrefix(l, prefix); j++ {
+			l = l[len(prefix):]
+		}
+
+		out += l
+	}
+
+	return
 }
--- stringc/stringc_test.go
+++ stringc/stringc_test.go
@@ -36,8 +36,65 @@ func TestIndent(t *testing.T) {
 	}
 }
 
+func TestDedent(t *testing.T) {
+	c := check.New(t)
+	c.Equal("test\nlines", Dedent("  test\n  lines", 1))
+}
+
+func TestDedentPrefix(t *testing.T) {
+	c := check.New(t)
+
+	tests := []struct {
+		p   string
+		in  string
+		out string
+		n   int
+	}{
+		{
+			p:   "",
+			in:  "  test\n  something\n  lines",
+			out: "test\nsomething\nlines",
+			n:   1,
+		},
+		{
+			p:   "   ",
+			in:  "    test\n    something\n    lines",
+			out: " test\n something\n lines",
+			n:   1,
+		},
+		{
+			p:   " * ",
+			in:  " * test\n * something\n * lines",
+			out: "test\nsomething\nlines",
+			n:   1,
+		},
+		{
+			p:   " ",
+			in:  "   test\n   something\n   lines",
+			out: " test\n something\n lines",
+			n:   2,
+		},
+		{
+			p:   "",
+			in:  "   test\n   something\n   lines",
+			out: "   test\n   something\n   lines",
+			n:   0,
+		},
+	}
+
+	for i, t := range tests {
+		c.Equal(t.out, DedentPrefix(t.in, t.p, t.n), "failed at %d", i)
+	}
+}
+
 func BenchmarkIndent(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Indent("\t", "test\nsomething\nlines")
+	}
+}
+
+func BenchmarkDedent(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		DedentPrefix("\t\t\ttest\n\t\t\tsomething\n\t\t\tlines", "\t", 3)
 	}
 }
