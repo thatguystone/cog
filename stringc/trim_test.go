733a75ae59ea3ce6ddf9343ab0e13d1a14f954fc
Andrew Stone <a@stoney.io>
2016-06-17 15:56:24 -0400

    stringc: Add Trim*Lines

---
+++ stringc/trim_test.go
@@ -0,0 +1,42 @@
+package stringc
+
+import (
+	"testing"
+
+	"github.com/thatguystone/cog/check"
+)
+
+func TestTrimLinesBasic(t *testing.T) {
+	c := check.New(t)
+
+	tests := []struct {
+		in  string
+		out string
+	}{
+		{
+			in:  "\n\ntests\n\n",
+			out: "tests",
+		},
+		{
+			in:  "    \n\ntests\n   \n   \n",
+			out: "tests",
+		},
+		{
+			in:  "  \nsome\nstuff\n",
+			out: "some\nstuff",
+		},
+		{
+			in:  "  \n  some  \nstuff  \n",
+			out: "  some  \nstuff  ",
+		},
+		{
+			in:  "  \n  \n  ",
+			out: "",
+		},
+	}
+
+	for i, test := range tests {
+		out := TrimLines(test.in)
+		c.Equal(test.out, out, "failed at %d", i)
+	}
+}
