68c628adb670f807f8a8750cc72c1e7b3f2d5836
Andrew Stone <a@stoney.io>
2018-06-05 15:23:11 -0700

    cfs: Remove useless junk

--- cfs/cfs.go
+++ cfs/cfs.go
@@ -2,55 +2,8 @@
 package cfs
 
 import (
-	"fmt"
-	"math/rand"
 	"os"
-	"path/filepath"
-	"strings"
 )
-
-// FindInParents climbs the directory tree, from the current directory to the
-// root, looking for a file with the given name at each level. If found, its
-// absolute path is returned.
-func FindInParents(file string) (path string, err error) {
-	return findInParents(file, true)
-}
-
-// FindDirInParents climbs the directory tree, from the current directory to
-// the root, looking for a directory with the given name at each level. If
-// found, its absolute path is returned.
-func FindDirInParents(dir string) (path string, err error) {
-	return findInParents(dir, false)
-}
-
-func findInParents(f string, wantsFile bool) (path string, err error) {
-	dir, err := os.Getwd()
-
-	if err == nil {
-		for dir != "/" {
-			path = filepath.Join(dir, f)
-
-			exists := false
-			if wantsFile {
-				exists, err = FileExists(path)
-			} else {
-				exists, err = DirExists(path)
-			}
-
-			if err != nil || exists {
-				break
-			}
-
-			dir = filepath.Join(dir, "..")
-		}
-	}
-
-	if err == nil && dir == "/" {
-		err = fmt.Errorf("could not find %s in parents", f)
-	}
-
-	return
-}
 
 func exists(path string) (exists, dir bool, err error) {
 	s, err := os.Stat(path)
@@ -77,55 +30,4 @@ func FileExists(path string) (bool, error) {
 func DirExists(path string) (bool, error) {
 	exists, dir, err := exists(path)
 	return exists && dir, err
-}
-
-// TempFile creates a new temporary file in /tmp/ (or equivalent) with the given
-// prefix and, unlike ioutil.TempFile, extension.
-func TempFile(prefix, ext string) (f *os.File, err error) {
-	return TempFileIn(os.TempDir(), prefix, ext)
-}
-
-// TempFileIn creates a new temporary file in the given directory with the given
-// prefix and, unlike ioutil.TempFile, extension.
-func TempFileIn(dir, prefix, ext string) (f *os.File, err error) {
-	if !strings.HasPrefix(ext, ".") {
-		ext = "." + ext
-	}
-
-	for i := 0; i < 10000; i++ {
-		name := filepath.Join(dir, fmt.Sprintf("%s%d%s",
-			prefix,
-			rand.Uint32(),
-			ext))
-
-		f, err = os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
-		if !os.IsExist(err) {
-			break
-		}
-	}
-
-	return
-}
-
-// ImportPath takes the given absolute path and returns the package that
-// contains the file, in "github.com/thatguystone/cog" form. If !isDir, then the
-// final path element is assumed to be a file and is stripped from the package
-// name.
-func ImportPath(absPath string, isDir bool) (string, error) {
-	paths := strings.Split(os.Getenv("GOPATH"), string(os.PathListSeparator))
-	for _, path := range paths {
-		if strings.HasPrefix(absPath, path) {
-			imp := strings.TrimPrefix(absPath, path)
-			imp = strings.Trim(imp, string(filepath.Separator))
-			imp = strings.TrimPrefix(imp, "src/")
-
-			if !isDir {
-				imp = filepath.Dir(imp)
-			}
-
-			return imp, nil
-		}
-	}
-
-	return "", fmt.Errorf("could not find %s in $GOPATH", absPath)
 }
--- cfs/cfs_test.go
+++ cfs/cfs_test.go
@@ -3,38 +3,11 @@ package cfs_test
 import (
 	"os"
 	"path/filepath"
-	"runtime"
 	"testing"
 
 	"github.com/thatguystone/cog/cfs"
 	"github.com/thatguystone/cog/check"
 )
-
-func TestFindInParents(t *testing.T) {
-	c := check.New(t)
-
-	_, err := cfs.FindInParents("idontexist")
-	c.NotNil(err)
-
-	_, err = cfs.FindInParents("README.md")
-	c.Nil(err)
-
-	_, err = cfs.FindInParents("cfs/cfs.go")
-	c.Nil(err)
-}
-
-func TestFindDirInParents(t *testing.T) {
-	c := check.New(t)
-
-	_, err := cfs.FindDirInParents("idontexist")
-	c.NotNil(err)
-
-	_, err = cfs.FindDirInParents("cog")
-	c.Nil(err)
-
-	_, err = cfs.FindDirInParents("cog/cfs")
-	c.Nil(err)
-}
 
 func TestFileExists(t *testing.T) {
 	c := check.New(t)
@@ -65,31 +38,4 @@ func TestDirExists(t *testing.T) {
 	ex, err = cfs.DirExists("/i/dont/exist/")
 	c.Must.Nil(err)
 	c.False(ex)
-}
-
-func TestTempFile(t *testing.T) {
-	c := check.New(t)
-
-	f, err := cfs.TempFile("tmp-", "aac")
-	c.Must.Nil(err)
-
-	defer func() {
-		f.Close()
-		os.Remove(f.Name())
-	}()
-
-	c.Contains(f.Name(), "tmp-")
-	c.Contains(f.Name(), ".aac")
-}
-
-func TestImportPath(t *testing.T) {
-	c := check.New(t)
-
-	_, err := cfs.ImportPath("does not exist", false)
-	c.Must.NotNil(err)
-
-	_, filename, _, _ := runtime.Caller(0)
-	path, err := cfs.ImportPath(filename, false)
-	c.Must.Nil(err, "filename=%s", filename)
-	c.Equal(path, "github.com/thatguystone/cog/cfs")
 }
